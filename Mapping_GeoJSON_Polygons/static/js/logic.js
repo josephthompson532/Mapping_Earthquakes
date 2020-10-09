d0265c7785bc2ecf794b96e4411928bee1245beb
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2020-10-09 14:19:39 -0700

    mapped out a polygon

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -18,7 +18,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": SatelliteStreets
+  "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
@@ -26,7 +26,7 @@ let map = L.map('mapid', {
   center: [43.7, -79.3],
   zoom: 11,
   layers: [satelliteStreets]
-})
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -35,11 +35,12 @@ L.control.layers(baseMaps).addTo(map);
 streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/josephthompson532/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/josephthompson532/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  // Creating a GeoJSON layer with the retieved data. 
+  L.geoJSON(data).addTo(map);
 });
