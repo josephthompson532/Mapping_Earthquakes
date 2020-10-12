065430d0011ecb0b5ad7422fc96e26a080d68e04
josephthompson532 <66881241+josephthompson532@users.noreply.github.com>
2020-10-11 18:56:12 -0700

    Made dynamically sized circleMarkers for Earthquakes spanning the globe

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,26 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
-
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [30.1975, -97.6664],
-  [43.6777, -79.6248],
-  [40.6413, -73.7781]
-];
-
-// Create a polyline using the line coordinates and make the line black.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
-
-
-L.circle([34.0522, -118.2437], {
-  radius: 100
-}).addTo(map);
-
+let map = L.map('mapid').setView([36.1733, -120.1794], 5);
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -32,16 +13,16 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-let cityData = cities;
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
 
-// Add code to create a marker for each city below and add it to the map
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000,
-    color: '#ff9933'
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+L.polyline(line, {
+  color: "yellow",
+  dashArray: "10,15",
+  dashOffset: "10"
+}).addTo(map);
+
